4594f46adade586695b18f5258d56a4b3a3cd977
Adrian Moya <moyaa1022@gmail.com>
2023-02-28 21:05:35 +0000

    feat(auth): basic sign in with two providers

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,5 @@
 <script lang="ts">
     import "../app.css";
-    import { auth } from '$lib/firebase';
-    import { browser } from "$app/environment";
-
-    let isLoggedIn = auth && auth.currentUser;
 </script>
 
 
