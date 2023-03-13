0a3ac709dadce2e4716e22d7919387cc5b40d40c
Adrian Moya <moyaa1022@gmail.com>
2023-03-13 14:16:10 -0400

    fix(api): added CORS header

--- src/routes/account/[uid]/subscriptionDetails/+server.ts
+++ src/routes/account/[uid]/subscriptionDetails/+server.ts
@@ -6,11 +6,14 @@ import { getStripeCustomerWithSubscriptions, getBlendProSubscription } from '$li
 export const GET = ( async ({ params: { uid } }) => {
   const customer = await getStripeCustomerWithSubscriptions(uid);
   if (!customer || customer?.deleted) {
-    throw error(404);
+    return new Response(JSON.stringify({
+      isSubscribedToBlendPro: false,
+      subscriptionPeriodEnd: 0,
+    }), { headers: { "Access-Control-Allow-Origin": "*" } });
   }
   const subscription = getBlendProSubscription(customer);
   return new Response(JSON.stringify({
     isSubscribedToBlendPro: !!subscription,
     subscriptionPeriodEnd: subscription?.current_period_end ?? 0,
-  }));
+  }), { headers: { "Access-Control-Allow-Origin": "*" } });
 }) satisfies RequestHandler;
