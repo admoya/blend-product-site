22ed661babe09e5c4a483e9bec72d1265301aa22
Adrian Moya <moyaa1022@gmail.com>
2023-03-14 15:57:56 +0000

    feat(data-deletion): added data deletion page

---
+++ src/routes/info/data-deletion/+page.svelte
@@ -0,0 +1,12 @@
+<h1>Data Deletion Request</h1>
+
+<div class="content">
+    <p>You can request to delete all data related to your account, including login information and stored decks. We will soon be releasing this functionality as part of the Account Management page, but for now, please send an email to <a href="mailto:blend-support@csma.technology">blend-support@csma.technology</a></p>
+</div>
+
+<style>
+    .content {
+        max-width: 75rem;
+        padding-bottom: 30rem;
+    }
+</style>
