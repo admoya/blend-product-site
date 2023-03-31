1f54249e7c986ec49ec3c1c91b44a06b4730f73d
Stephanie Cruz <exscruzme@gmail.com>
2023-03-31 11:19:53 -0400

    feat(subscribe): fixed layout on subscribe page

--- src/routes/subscribe/+page.svelte
+++ src/routes/subscribe/+page.svelte
@@ -4,7 +4,7 @@
 
 <form method="post" action="https://communications.blendreading.com/subscription/form" class="listmonk-form">
     <div>
-        <h2>Join our mailing list to be notified when we launch! &#127881;</h2>
+        <h2>Join our mailing list to be notified of updates and Blend News! &#127881;</h2>
         <input type="hidden" name="nonce" />
         <p><input type="email" name="email" required placeholder="E-mail" /></p>
         <p><input type="text" name="name" placeholder="Name (optional)" /></p>
@@ -12,12 +12,17 @@
         <p>
           <input id="8c1c5" type="hidden" name="l" value="8c1c54b6-ed85-4eb0-bdb5-5a3c385c8560" />
         </p>
-
         <p><input type="submit" value="Subscribe" class="btn" /></p>
     </div>
 </form>
 
 <style>
+    .listmonk-form {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding: 10%;
+    }
     input[type="text"], input[type="email"]{
         width: 50%;
         padding: 10px 8px;
@@ -30,9 +35,12 @@
         font-family: "Heebo";
         font-weight: bold;
         font-size: 1.5em;
+        margin: 0;
     }
     p {
         text-align: center;
+        display: flex;
+        justify-content: center;
     }
     h2 {
         font-family: "Contrail One";
