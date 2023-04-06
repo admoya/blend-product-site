dff45ced3a27dcdbdf7e91d60d09269ffc1344e1
Stephanie Cruz <exscruzme@gmail.com>
2023-04-06 17:35:27 -0400

    feat(sucess): add custom success page

---
+++ src/routes/help/report-an-issue/success/+page.svelte
@@ -0,0 +1,14 @@
+<svelte:head>
+  <title>Report an Issue</title>
+</svelte:head>
+<div class="content">
+  <h1>Thank you for submitting your issue! We will reach out to you shortly.</h1>
+</div>
+
+<style>
+  .content {
+    justify-content: center;
+    margin: 5rem auto;
+    height: 25rem;
+  }
+</style>
