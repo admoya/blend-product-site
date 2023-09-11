8a3525db6769bdbc01f72008cf3c7b9921498c4d
Stephanie Cruz <stephanie@csma.technology>
2023-09-11 11:02:02 -0400

    feat(sor-blog): updated links and images in SOR blog post (#59)
    updated some links

---
+++ src/lib/data/blogs/align-teaching-with-the-science-of-reading/index.ts
@@ -0,0 +1,165 @@
+import thumbnail from './images/sor-thumbnail.png';
+import pillars from './images/pillars-of-sor.png';
+import organizations from './images/organizations.png';
+const post: Blog.BlogPost = {
+  slug: 'align-teaching-with-the-science-of-reading',
+  summary: 'Adapting your lessons to fit the science of reading methodology can be overwhelming. We are here to help with lots of tips and resources!',
+  title: 'How to Align Your Teaching with the Science of Reading',
+  previewImg: {
+    type: 'image',
+    src: thumbnail,
+    altText: 'Science of Reading Thumbnail',
+  },
+  blocks: [
+    {
+      type: 'text',
+      content: `The <a href="https://time.com/6205084/phonics-science-of-reading-teachers/" target="_blank" rel="noopener noreferrer">science of reading movement</a> has been sweeping across the nation for the last few years, and with it is coming <a href="https://www.edweek.org/teaching-learning/which-states-have-passed-science-of-reading-laws-whats-in-them/2022/07" target="_blank" rel="noopener noreferrer">new legislation in many states</a> to reform literacy instruction to align with evidence-based practices. Given that the science of reading encompasses many different components of teaching and learning to achieve the goal of literacy in children, it can be very overwhelming for teachers to get started in changing their lessons to use the techniques outlined in the methodology.`,
+    },
+    {
+      type: 'image',
+      src: pillars,
+      align: 'left',
+      altText: 'Infographic showing the five pillars of the science of reading',
+      size: {
+        width: 400,
+      },
+      caption: 'The five pillars of the science of reading',
+    },
+    {
+      type: 'text',
+      content: `We’re here to help, with some tips on what you can do to better <a href="https://read-write-grow.myshopify.com/blogs/news/beginning-with-science-of-reading" target=_blank rel="noopener noreferrer">align your teaching</a> with the science of reading (SOR), and get started right away even while you may still be learning about the five pillars of SOR. It can be daunting to make so many changes that check all of the boxes in this approach for teaching kids how to read, but with the information in this post, you can start making small changes that will add up to big results for your students.`,
+    },
+    {
+      type: 'heading',
+      size: 2,
+      content: `What is the science of reading?`,
+    },
+    {
+      type: 'text',
+      content: `The <a href=https://www.lexialearning.com/blog/a-full-breakdown-of-the-science-of-reading-components target="_blank" rel="noopener noreferrer">science of reading</a> is a term that refers to a body of research that studies how the brain learns how to read, drawing from a few different fields, and ultimately shows which methods are the most effective in teaching children how to read. There’s a lot more to it than what we’ll talk about here, and there are lots of courses and certifications that you can take to delve into the entire approach, but what it really breaks down to is five pillars that scientists believe are the key to building literacy in children. `,
+    },
+    {
+      type: 'text',
+      content: `The five pillars of SOR are <b>phonemic awareness, phonics, fluency, vocabulary, and reading comprehension</b>.`,
+    },
+    {
+      type: 'text',
+      content: `These key components may already appear in your teaching today, but what SOR emphasizes is that these skills should be taught systematically to your students, rather than in a more ad hoc approach. These pillars should be focused on individually and work together to build a strong foundation for children to fundamentally understand how to decode and comprehend words.`,
+    },
+    {
+      type: 'heading',
+      size: 2, 
+      content: `What does this mean for my teaching?`,
+    },
+    {
+      type: 'text',
+      content: `In theory, all of the above sounds great, but in practice, it can be hard to adapt and try to address all five pillars in your teaching. While it may take time to completely overhaul your classroom if that’s what’s necessary, here are some tips that we think can help you get started in a manner that feels more manageable and less like boiling the ocean.`,
+    },
+    {
+      type: 'text',
+      content: `<br/><h2>The Do's of SOR</h2>`,
+    },
+    {
+      type: 'text',
+      content: `<b>Make sure your kids are keeping their eyes on the words as they’re reading.</b> It’s important to emphasize the
+      skill of decoding the words, rather than memorizing them by sound or sight. You can even encourage them to slide
+      their finger under the word as they sound it out – this is a great practice especially for younger students!`,
+    },
+    {
+      type: 'text',
+      content: `<b>Focus on building fluency at the word level</b> before expecting it at the sentence or passage level. 
+      This is where a blending board can come in handy, allowing you to focus on phonemic awareness and morphology through which your students can 
+      learn how to sound out and infer meaning from the different parts in a word. <a href="/features">Our digital blending board</a> is a great place to start – and it’s free!`
+    },
+    {
+      type: 'text',
+      content: `<b>Build vocabulary and spelling skills by introducing morphology early.</b> 
+      This is the study of different parts of words that contain meaning, such as common prefixes, suffixes, and roots. 
+      There are plenty of resources available on our website to help you with morphology teaching -- make sure to <a href="/blog" target="_blank">check out our other blog posts</a>!`,
+    },
+    {
+      type: 'text',
+      content: `<b>Introduce new concepts systematically, bringing in one grapheme at a time.</b> Then engage in frequent review of previously taught skills so that students begin to consolidate all the patterns. 
+      Using Blend’s completely customizable decks, you can control the grapheme sets and adapt your blending boards to your students’ skills as they develop over time!`,
+    },
+    {
+      type: 'text',
+      content: `<b>For small group instruction or intervention, assess students to find out what gaps they may have</b> and make instructional decisions that are prescriptive to their needs. 
+      A great place to start is with the <a href="https://esu11.org/wp-content/uploads/sites/13/2010/11/Phonics_Survey.pdf" target="_blank" rel="noopener noreferrer">CORE Phonics survey</a>
+      and the <a href="https://dibels.uoregon.edu/materials" target="_blank" rel="noopener noreferrer">DIBELS fluency probes</a>, both of which are free!`,
+    },
+    {
+      type: 'text',
+      content: `<b>Continue building background knowledge, vocabulary, and listening comprehension</b> by reading rich texts aloud to your students that may be above their fluency level.`,
+    },
+    {
+      type: 'text',
+      content: `<h2>The Don't's of SOR</h2>`,
+    },
+    {
+      type: 'text',
+      content: `<b>Avoid the <a href="https://fivefromfive.com.au/phonics-teaching/the-three-cueing-system/" target="_blank" rel="noopener noreferrer">3-cueing system</a></b> or any curriculum that advises students to use picture clues or guess based on the first letter. 
+      These practices tend to encourage bad habits in students like memorizing patterns and certain words, rather than developing the ability to decode new words.`,
+    },
+    {
+      type: 'text',
+      content: `<b>Avoid teaching sight words using flash cards or whole word memorization.</b> Instead, for irregular words like “there” or “through”, identify with your students the parts of the word that are decodable and the parts that are not.`,
+    },
+    {
+      type: 'text',
+      content: `<b>Avoid using “leveled” readers</b> that contain words that students cannot yet decode or have in their sight vocabulary. Instead, try using <a href="https://www.readingrockets.org/topics/curriculum-and-instruction/articles/what-are-decodable-books-and-why-are-they-important" target="_blank" rel="noopener noreferrer">decodable texts</a> that are controlled for the skills you’re working on. 
+      There are tons of free resources out there for you to get started like the <a href="https://ufli.education.ufl.edu/foundations/toolbox/" target="_blank" rel="noopener noreferrer">UFLI Toolbox</a> or 
+      <a href="https://portal.flyleafpublishing.com/" target="_blank" rel="noopener noreferrer">Flyleaf Publishing’s Free E-Book Library</a>.`,
+    },
+    {
+      type: 'heading',
+      size: 2,
+      content: `We’re all in this together.`,
+    },
+    {
+      type: 'text',
+      content: `Last but not least, we want to encourage you to lean on your fellow teachers and work together to come up with ways to make these changes in your classrooms. Not only is it important to be consistent with these practices to reinforce these skills with your students, it’s also more manageable and enjoyable to tackle it together! `,
+    },
+    {
+      type: 'text',
+      content: `Blend now offers group licenses so all of the teachers in your organization can have access to the Pro features together. This is great for school settings so teachers can use the same tool in different classrooms, and so that you can share your resources with each other! With group licenses, decks and playlists can be shared across the organization, so that you can collaborate on your blending drills and save time when lesson planning. If you’re interested, you can <a href="https://drive.google.com/file/d/1HQoOfhXIJPaVRFmd8GBM7DMjLgD_YfPx/view?usp=drive_link" target="_blank" rel="noopener noreferrer">click here for more details!</a`,
+    },
+    {
+      type: 'image',
+      src: organizations,
+      align: 'left',
+      altText: 'Screenshots of Blend showing organization management and shared playlists',
+      caption: 'Group licenses in Blend allow you to share decks and playlists with your whole organization',
+    },
+    {
+      type: 'heading',
+      size: 2,
+      content: `You've got this!`,
+    },
+    {
+      type: 'text',
+      content: `We hope this post was helpful in getting you started to align your teaching with the science of reading. We know you have a lot on your plate in terms of teaching and caring for your students, and we at Blend want to do everything we can to help ease your experiences in the classroom and planning for your lessons, so that you can focus on building strong foundational reading skills in your students and watch them succeed!`,
+    },
+    {
+      type: 'text',
+      content: `<h2>Resources in this article</h2>
+      <ul style="text-align: left">
+        <li><a href="https://time.com/6205084/phonics-science-of-reading-teachers/" target="_blank">TIME Magazine: Inside the Massive Effort to Change the Way Kids Are Taught to Read</a></li>
+        <li><a href="https://www.edweek.org/teaching-learning/which-states-have-passed-science-of-reading-laws-whats-in-them/2022/07" target="_blank">Education Week: Which States Have Passed 'Science of Reading' Laws? What's in Them?</a></li>
+        <li><a href="https://read-write-grow.myshopify.com/blogs/news/beginning-with-science-of-reading" target="_blank">Read Write Grow: 5 Ways to Begin Aligning to Science of Reading</a></li>
+        <li><a href="https://www.lexialearning.com/blog/a-full-breakdown-of-the-science-of-reading-components" target="_blank">Lexia Learning: A Full Breakdown of the Science of Reading Components</a></li>
+        <li><a href="https://blendreading.com/features" target="_blank">Blend Reading: Blend Features</a></li>
+        <li><a href="https://blendreading.com/blog" target="_blank">Blend Reading: The Blend Blog</a></li>
+        <li><a href="https://esu11.org/wp-content/uploads/sites/13/2010/11/Phonics_Survey.pdf" target="_blank">ESU11: CORE Phonics Survey</a></li>
+        <li><a href="https://dibels.uoregon.edu/materials" target="_blank">DIBELS: DIBELS 8th Edition Materials</a></li>
+        <li><a href="https://fivefromfive.com.au/phonics-teaching/the-three-cueing-system/" target="_blank">Five From Five: The three cueing system</a></li>
+        <li><a href="https://www.readingrockets.org/topics/curriculum-and-instruction/articles/what-are-decodable-books-and-why-are-they-important" target="_blank">Reading Rockets: What Are Decodable Books and Why Are They Important?</a></li>
+        <li><a href="https://ufli.education.ufl.edu/foundations/toolbox/" target="_blank">UFLI: UFLI Foundations Toolbox</a></li>
+        <li><a href="https://portal.flyleafpublishing.com/" target="_blank">Flyfleaf Publishing: Free E-Book Library</a></li>
+        <li><a href="https://drive.google.com/file/d/1HQoOfhXIJPaVRFmd8GBM7DMjLgD_YfPx/view" target="_blank">Blend Reading: Blend Organization License Flyer</a></li>
+      </ul>`,
+    },
+  ],
+};
+
+export default post;
--- src/lib/data/blogs/index.ts
+++ src/lib/data/blogs/index.ts
@@ -3,5 +3,6 @@ import gettingStarted from './getting-started-with-blend';
 import blend2Overview from './blend-2_0-overview';
 import playlistsTutorial from './playlists-tutorial';
 import blendableSoundsTutorial from './blendable-sounds-tutorial';
+import alignWithSor from './align-teaching-with-the-science-of-reading';
 
-export default [whatIsABlendingBoard, gettingStarted, blend2Overview, playlistsTutorial, blendableSoundsTutorial];
+export default [alignWithSor, whatIsABlendingBoard, gettingStarted, blend2Overview, playlistsTutorial, blendableSoundsTutorial];
