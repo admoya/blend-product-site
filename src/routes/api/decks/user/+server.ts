29e651166b8e79fe4f211b30a2402d8793a8dcb6
Stephanie Cruz <stephanie@csma.technology>
2024-02-19 15:19:39 -0500

    feat(decks-api-filter): allow query params to filter user decks response (#84)
    * feat(org-details): return organization info in user api
    
    * feat(user-decks-params): implement query params for filtering user decks
    
    * fix(PR): refactored query params to omitOrgDecks and orgId
    
    * fix(spacing): fixed weird indents

--- src/routes/api/decks/user/+server.ts
+++ src/routes/api/decks/user/+server.ts
@@ -1,33 +1,61 @@
-import { authenticate, getOrganizationDecks, getOrganizationInfo, getUserData, getUserOrganizations, readPath } from '$lib/server/firebaseUtils';
+import { authenticate, getOrganizationDecks, getOrganizationInfo, getUserOrganizations, readPath } from '$lib/server/firebaseUtils';
 import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
 export const GET = (async (event) => {
-  let organizationDeckArray: Database.Deck[] = [];
   const { uid } = await authenticate(event);
-  const decks = (await readPath<Database.Decks.User>(`/decks/user/${uid}`)) || {};
-  const deckArray = Object.entries(decks).map(([key, val]) => val);
-  const positionOffset = Math.max(...deckArray.map(({ position }) => position), 0) + 2; // +2 because legacy position can be -1, and we want the new position to be strictly greater TODO: refactor this once we get rid of the legacy magic numbers in positioning
-  const organizationIds = await getUserOrganizations(uid);
-  organizationDeckArray = (
-    await Promise.all(
-      organizationIds.map(async (orgId) => {
-        const decks = (await getOrganizationDecks(orgId)) ?? {};
-        const orgInfo = await getOrganizationInfo(orgId);
-        return Object.values(decks).map(({ deck }) => ({
-          ...deck,
-          position: deck.position + positionOffset,
-          orgSource: {
-            orgName: orgInfo?.name,
-            orgId,
-          },
-        }));
-      }),
-    )
-  ).flat();
-  return json([...deckArray, ...organizationDeckArray], {
-    headers: [['Access-Control-Allow-Origin', '*']],
-  });
+  const omitOrgDecks = event.url.searchParams.get('omitOrgDecks') ?? false;
+  const orgIdParam = event.url.searchParams.get('orgId');
+  if (omitOrgDecks && orgIdParam)
+    return new Response('Cannot specify both omitOrgDecks and orgId', { status: 400, headers: [['Access-Control-Allow-Origin', '*']] });
+  if (omitOrgDecks) {
+    const userDecks = (await readPath<Database.Decks.User>(`/decks/user/${uid}`)) || {};
+    const deckArray = Object.entries(userDecks).map(([key, val]) => val);
+    return json(deckArray, {
+      headers: [['Access-Control-Allow-Origin', '*']],
+    });
+  } else if (orgIdParam) {
+    const organizationIds = await getUserOrganizations(uid);
+    if (!organizationIds.includes(orgIdParam))
+      return new Response('User is not a member of this organization', { status: 403, headers: [['Access-Control-Allow-Origin', '*']] });
+    const orgDecks = (await getOrganizationDecks(orgIdParam)) ?? {};
+    const organizationInfo = await getOrganizationInfo(orgIdParam);
+    const deckArray = Object.values(orgDecks).map(({ deck }) => ({
+      ...deck,
+      orgSource: {
+        orgName: organizationInfo?.name,
+        orgId: orgIdParam,
+      },
+    }));
+    return json(deckArray, {
+      headers: [['Access-Control-Allow-Origin', '*']],
+    });
+  } else {
+    const userDecks = (await readPath<Database.Decks.User>(`/decks/user/${uid}`)) || {};
+    const userDeckArray = Object.entries(userDecks).map(([key, val]) => val);
+    const positionOffset = Math.max(...userDeckArray.map(({ position }) => position), 0) + 2; // +2 because legacy position can be -1, and we want the new position to be strictly greater TODO: refactor this once we get rid of the legacy magic numbers in positioning
+    const organizationIds = await getUserOrganizations(uid);
+    const organizationDeckArray = (
+      await Promise.all(
+        organizationIds.map(async (orgId) => {
+          const orgDecks = (await getOrganizationDecks(orgId)) ?? {};
+          const orgInfo = await getOrganizationInfo(orgId);
+          return Object.values(orgDecks).map(({ deck }) => ({
+            ...deck,
+            position: deck.position + positionOffset,
+            orgSource: {
+              orgName: orgInfo?.name,
+              orgId,
+            },
+          }));
+        }),
+      )
+    ).flat();
+
+    return json([...userDeckArray, ...organizationDeckArray], {
+      headers: [['Access-Control-Allow-Origin', '*']],
+    });
+  }
 }) satisfies RequestHandler;
 
 export const OPTIONS = (() => {
--- src/routes/api/user/+server.ts
+++ src/routes/api/user/+server.ts
@@ -1,15 +1,31 @@
-import { authenticate, getUserData } from '$lib/server/firebaseUtils';
-import { getBlendProSubscription, getStripeCustomerWithSubscriptions, isOrganizationMember, isSubscribedToBlendPro } from '$lib/server/subscriptionUtils';
+import { authenticate, getOrganizationInfo, getOrganizationMemberDetails, getUserData, getUserOrganizations } from '$lib/server/firebaseUtils';
+import {
+  getBlendProSubscription,
+  getStripeCustomerWithSubscriptions,
+  isOrganizationMember,
+  isSubscribedToBlendPro,
+} from '$lib/server/subscriptionUtils';
 import type { RequestHandler } from './$types';
 
 export const GET = (async (event) => {
   const { uid } = await authenticate(event);
   const [stripeCustomer, firebaseUserData] = await Promise.all([getStripeCustomerWithSubscriptions(uid), getUserData(uid)]);
   const subscriptionData = stripeCustomer && !stripeCustomer.deleted && getBlendProSubscription(stripeCustomer);
+  const userOrganizations = await getUserOrganizations(uid);
+  const organizationInfo = await Promise.all(
+    userOrganizations.map(async (orgId) => {
+      const orgInfo = await getOrganizationInfo(orgId);
+      return {
+        orgName: orgInfo?.name,
+        orgId,
+      };
+    }),
+  );
   const userData = {
     ...firebaseUserData,
     isSubscribedToBlendPro: (await isOrganizationMember(uid)) || isSubscribedToBlendPro(stripeCustomer),
     subscriptionPeriodEnd: subscriptionData ? subscriptionData.current_period_end : 0,
+    organizationInfo,
   };
   return new Response(JSON.stringify(userData, null, 2), {
     headers: [['Access-Control-Allow-Origin', '*']],
