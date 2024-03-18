800ddd51237ee4e9710549d334486bc817eb7da6
Stephanie Cruz <stephanie@csma.technology>
2024-03-17 21:06:38 -0500

    feat(add-library): added old preloaded decks to library (#91)

--- src/lib/data/marketplaceDecks.ts
+++ src/lib/data/marketplaceDecks.ts
@@ -3,6 +3,8 @@ import doublingPractice from '$lib/assets/marketplace/doubling-practice.png';
 import weldedSounds from '$lib/assets/marketplace/welded-sounds.png';
 import latin from '$lib/assets/marketplace/latin.png';
 import twosyllable from '$lib/assets/marketplace/2syllable.png';
+import cvcMagicE from '$lib/assets/marketplace/cvc-and-magic-e.jpg';
+import blendsAdvancedVowels from '$lib/assets/marketplace/blends-and-advanced-vowels.jpg';
 
 export type MarketplaceDeck = {
   name: string;
@@ -16,7 +18,7 @@ const marketplaceDecks: MarketplaceDeck[] = [
   {
     name: '2 Syllables (6 cards)',
     description:
-      "This deck features six columns and is great for introducing 2 syllable words and syllable division patterns to your students. Create VC/CV words or collapse the 3rd column to change them to V/CV and VC/V.",
+      'This deck features six columns and is great for introducing 2 syllable words and syllable division patterns to your students. Create VC/CV words or collapse the 3rd column to change them to V/CV and VC/V.',
     author: 'Summer Kiesel',
     image: twosyllable,
     id: '-Nl4-F3yj7JTYznUkUXL',
@@ -51,6 +53,21 @@ const marketplaceDecks: MarketplaceDeck[] = [
     author: 'Summer Kiesel',
     image: latin,
     id: '-NarqpewAp2p8H6inYXL',
+  },
+  {
+    name: 'CVC and Magic E',
+    description: 'This is one of our old preloaded decks. It contains basic graphemes for CVC words with the option to add in Magic E.',
+    author: 'Summer Kiesel',
+    image: cvcMagicE,
+    id: '-NtE1MN0Cf_aInFK3FtP',
+  },
+  {
+    name: 'Blends and Advanced Vowels',
+    description:
+      'This is one of our old preloaded decks. It contains basic graphemes for some consonant blends and more advanced vowels. The blends on this board are separated.',
+    author: 'Summer Kiesel',
+    image: blendsAdvancedVowels,
+    id: '-NtE1YEqHfSFUzdkeLzM',
   },
 ];
 
