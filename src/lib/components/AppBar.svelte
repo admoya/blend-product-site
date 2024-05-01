c4561f3645573d532692cc100571852eaa62af87
Stephanie Cruz <stephanie@csma.technology>
2024-04-30 23:45:38 -0500

    feat(faq): added a help page with faqs (#102)

--- src/lib/components/AppBar.svelte
+++ src/lib/components/AppBar.svelte
@@ -19,14 +19,17 @@
       <li>
         <a id="app" href={$appUrl} target="_blank" rel="noopener noreferrer" on:click={() => window.gtag('event', 'open_app')}>app</a>
       </li>
-      <li>
+      <!-- <li>
         <a id="home" href="/">home</a>
-      </li>
+      </li> -->
       <li>
         <a id="pricing" href="/pricing">pricing</a>
       </li>
       <li>
         <a id="organization" href="/organization">organizations</a>
+      </li>
+      <li>
+        <a id="help" href="/help">help</a>
       </li>
       <li>
         <a id="library" href="/library">library</a>
@@ -111,7 +114,7 @@
     color: white;
   }
 
-  a#home:hover {
+  a#help:hover {
     color: #f1b4fb;
   }
 
