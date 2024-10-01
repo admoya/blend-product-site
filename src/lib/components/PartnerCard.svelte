3bfca376f4d7e91a1cfcc55020a5fd692ab67a86
Adrian Moya <adrian@csma.technology>
2024-10-01 18:50:26 +0200

    feat(partners): added partners to home (#133)
    * feat(partners): added partners to home
    
    * feat(logo): updated logos
    
    * fix(rcf): update rcf logo, remove ontiveros
    
    * fix(sage): update sage logo to higher res
    
    * fix(img): remove img max width;
    
    ---------
    
    Co-authored-by: Adrian Moya <adrian@csma.tehcnology>
    Co-authored-by: Stephanie Cruz <exscruzme@gmail.com>

---
+++ src/lib/components/PartnerCard.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+  export let name: string;
+  export let logo: string;
+  export let url: string;
+</script>
+
+<div class="logo-container">
+  <a href={url} target="_blank" title={name}>
+    <img src={logo} alt={`${name} logo`} />
+  </a>
+</div>
+
+<style>
+  .logo-container {
+    border-radius: 100%;
+    background-color: rgb(253, 249, 241);
+    padding: 0.3rem;
+    width: 10rem;
+    height: 10rem;
+    box-shadow: 3px 4px 4px 0px black;
+    transition: transform 0.3s ease-in-out;
+  }
+
+  @media (max-width: 480px) {
+    .logo-container {
+      max-width: 7rem;
+      max-height: 7rem;
+    }
+  }
+
+  a {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
+  }
+
+  img {
+    max-width: 100%;
+    max-height: 100%;
+  }
+
+  .logo-container:hover {
+    transform: scale(1.1);
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,14 @@
   import play from '$lib/assets/home/play.png';
   import edit from '$lib/assets/home/edit.png';
   import playlists from '$lib/assets/home/playlists-side-by-side.png';
+
+  import sageLogo from '$lib/assets/home/partner-logos/sage-logo.png';
+  import slantLogo from '$lib/assets/home/partner-logos/slant-logo.png';
+  import reedLogo from '$lib/assets/home/partner-logos/reed-logo.png';
+  import forestCityLogo from '$lib/assets/home/partner-logos/forest-city-logo.webp';
+
   import { appUrl } from '$lib/utils';
+  import PartnerCard from '$lib/components/PartnerCard.svelte';
 </script>
 
 <svelte:head>
@@ -110,6 +117,34 @@
     </div>
   </div>
 </section>
+<section id="partners" class="feature" style="background: #3b2e86;">
+  <div class="feature-title"><h1>Trusted by the best</h1></div>
+  <div class="inner-content">
+    <div class="short-description">
+      <p>
+        We are proud to partner with leading literacy programs, schools, and districts around the country to provide the absolute best phonics
+        experience for students and educators!
+      </p>
+    </div>
+    <div class="partner-logos">
+      <PartnerCard name="Sage Literacy" logo={sageLogo} url="https://www.sageliteracy.org/" />
+      <PartnerCard name="Slant System" logo={slantLogo} url="https://www.slantsystem.com/" />
+      <PartnerCard name="Reed Charitable Foundation" logo={reedLogo} url="https://reedcharitablefoundation.org/" />
+      <PartnerCard name="Forest City Elementary" logo={forestCityLogo} url="https://www.forestcity.k12.ia.us/home" />
+    </div>
+    <div class="short-description">
+      <h2 style="text-align: center;">Want to join the team?</h2>
+      <p>
+        If your organization is looking to enhance your phonics instruction, we'd love to work with you to empower your teachers and fast-track your
+        students!
+      </p>
+      <div class="row flex-center flex-wrap">
+        <a class="btn" href="/organization"><h2>learn more</h2></a>
+        <a class="btn" href="/organization#form"><h2>get an estimate</h2></a>
+      </div>
+    </div>
+  </div>
+</section>
 <section class="epilogue">
   <h1>Ready to get started?</h1>
   <p>See our pricing plans and try Blend now!</p>
@@ -188,7 +223,7 @@
   }
 
   .description {
-    width: 30%;
+    max-width: 70rem;
     margin: 0px;
     text-align: justify;
     flex-basis: 40rem;
@@ -199,6 +234,27 @@
   h2 {
     margin-top: 1rem;
     margin-bottom: 1rem;
+  }
+
+  .short-description {
+    max-width: 60rem;
+    margin: 0px;
+    text-align: justify;
+    text-align-last: center;
+  }
+
+  .side-by-side > .description {
+    width: 30%;
+  }
+
+  .partner-logos {
+    display: flex;
+    flex-direction: row;
+    gap: 3rem;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    width: 100%;
+    margin: 2rem 0;
   }
 
   .feature {
